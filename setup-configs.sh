1bbdbf95d624faff6854db41e604a146aa3736a8
Alex Bishop <alexbishop1234@gmail.com>
2024-07-20 00:42:52 +0200

    Updates kitty and installs alacritty
    Maybe kitty isn't the best. (I'm not sold on a terminal that's written
    in Python.) So, let's change to alacritty.

--- setup-configs.sh
+++ setup-configs.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/sh
 
-for f in foot lynx nvim sway tmux waybar kitty ; do
+for f in foot lynx nvim sway tmux waybar kitty alacritty ; do
   if [ ! -e ~/.config/$f ]
   then
     ln -s "$(pwd)/$f" ~/.config/$f
   fi
 done
 
-for f in .clang-format .clangd .dircolors .bash_aliases ; do
+for f in .clang-format .clangd .dircolors .bash_aliases ".local/bin/alacritty-new-window" ; do
   if [ ! -e ~/$f ]
   then
     ln -s "$(pwd)/misc/$f" ~/$f
