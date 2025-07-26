62f9ef578c29621f94e5095166cef01fba86a610
Alex Bishop <alexbishop1234@gmail.com>
2025-07-26 16:06:27 +1000

    removes some cmp plugins that were causing trouble

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -62,15 +62,15 @@ pckr.add({
     requires = {
       -- lsp stuff
       "hrsh7th/nvim-cmp",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-path",
+       -- "hrsh7th/cmp-nvim-lsp",
+       -- "hrsh7th/cmp-path",
 
       -- mason
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
 
       -- snippets
-      "hrsh7th/cmp-buffer",
+      -- "hrsh7th/cmp-buffer",
       "saadparwaiz1/cmp_luasnip",
       "L3MON4D3/LuaSnip"
     },
